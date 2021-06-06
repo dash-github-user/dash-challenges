0665265d08b98b96f49a4b16a85936fd8777320e
Vickey Chen <vickeychen@cvpcorp.com>
2021-06-06 10:03:59 -0400

    update name

--- terraform/dev/dash_rds.tf
+++ terraform/dev/dash_rds.tf
@@ -1,4 +1,5 @@
-# DC1-21 Configure Back-End Infrastructureresource "aws_glue_connection" "iqies_connector" {
+# DC1-21 Configure Back-End Infrastructure
+resource "aws_glue_connection" "dash_connector" {
   connection_properties = {
     JDBC_CONNECTION_URL = "${data.aws_ssm_parameter.dash_connection_string.value}"
     PASSWORD            = "${data.aws_ssm_parameter.dash_rds_password.value}"
